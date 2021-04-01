ee547608ca5c3a9fa23ded201d208f0b3387afb8
Ozkan Sezer <sezeroz@gmail.com>
2021-04-01 21:40:50 +0300

    fixed 'missing initializer' warnings.

--- src/rvoice/fluid_rvoice_event.c
+++ src/rvoice/fluid_rvoice_event.c
@@ -132,7 +132,9 @@ fluid_rvoice_eventhandler_push(fluid_rvoice_eventhandler_t* handler,
                                fluid_real_t realparam)
 {
     fluid_rvoice_event_t* event;
-    fluid_rvoice_event_t local_event = { 0 };
+    fluid_rvoice_event_t local_event;
+
+    FLUID_MEMSET(&local_event, 0, sizeof(local_event));
     event = handler->is_threadsafe ?
             fluid_ringbuffer_get_inptr(handler->queue, handler->queue_stored) : &local_event;
 
@@ -158,7 +160,9 @@ fluid_rvoice_eventhandler_push_ptr(fluid_rvoice_eventhandler_t* handler,
                                    void* method, void* object, void* ptr)
 {
     fluid_rvoice_event_t* event;
-    fluid_rvoice_event_t local_event = { 0 };
+    fluid_rvoice_event_t local_event;
+
+    FLUID_MEMSET(&local_event, 0, sizeof(local_event));
     event = handler->is_threadsafe ?
             fluid_ringbuffer_get_inptr(handler->queue, handler->queue_stored) : &local_event;
 
@@ -185,7 +189,9 @@ fluid_rvoice_eventhandler_push5(fluid_rvoice_eventhandler_t* handler,
                                 fluid_real_t r3, fluid_real_t r4, fluid_real_t r5)
 {
     fluid_rvoice_event_t* event;
-    fluid_rvoice_event_t local_event = { 0 };
+    fluid_rvoice_event_t local_event;
+
+    FLUID_MEMSET(&local_event, 0, sizeof(local_event));
     event = handler->is_threadsafe ?
             fluid_ringbuffer_get_inptr(handler->queue, handler->queue_stored) : &local_event;
 
