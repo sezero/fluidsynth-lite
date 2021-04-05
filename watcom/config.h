3f1e02bafcfe92bc4f4d539b131e8929e03040b6
Ozkan Sezer <sezeroz@gmail.com>
2021-04-05 11:01:40 +0300

    tiny config update for watcom

--- watcom/config.h
+++ watcom/config.h
@@ -107,7 +107,7 @@
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
 #ifndef __cplusplus
-/* #undef inline */
+#define __inline
 #endif
 
 #endif /* CONFIG_H */
