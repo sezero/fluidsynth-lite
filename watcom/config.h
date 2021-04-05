caceb012149e3da53bf16fd0d7eb9301d89a1cd5
Ozkan Sezer <sezeroz@gmail.com>
2021-04-05 11:56:28 +0300

    fix typo in previous commit

--- watcom/config.h
+++ watcom/config.h
@@ -107,7 +107,7 @@
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
 #ifndef __cplusplus
-#define __inline
+#define inline __inline
 #endif
 
 #endif /* CONFIG_H */
